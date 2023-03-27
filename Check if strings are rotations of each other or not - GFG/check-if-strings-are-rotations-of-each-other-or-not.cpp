08bc4ffba715e6b93391c2389b4307c7a4701558
Ankur Mishra <ankurmisra01@gmail.com>
2023-03-28 02:22:15 +0530

    Added solution - LeetHub

---
+++ Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        int n = s1.size();
+        for(int i=0;i<n;i++)
+        {
+            string a= "";
+            a+=s1[n-1];
+            s1.pop_back();
+            s1.insert(0, a);
+            a="";
+            if(s1 == s2) return 1;
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
